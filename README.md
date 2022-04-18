242874f8665227e3a6045832145aabf4a742f4c6
Geoffrey McRae <geoff@hostfission.com>
2022-04-18 13:45:34 +1000

    Initial code check-in

--- README.md
+++ README.md
@@ -1,2 +1,145 @@
 # SteamQueryProxy
-Valve Steam Query reverse proxy using NFQUEUE to intercept and cache the Steam Query protocol for misbehaving dedicated servers.
+
+Valve Steam Query reverse proxy using NFQUEUE to intercept and cache the Steam
+Query protocol for misbehaving dedicated servers.
+
+https://developer.valvesoftware.com/wiki/Server_queries
+
+# Rationale
+
+Some dedicated servers under linux have in-built flood protection on for the
+Steam Query Port protocol that can not be adjusted or disabled. This causes
+issues with websites and services that monitor the server and collect metrics,
+causing the server to appear "offline" when this protection is triggered.
+
+This application makes use of netfilter NFQUEUE to intercept the Steam Query
+protocol datagrams and reply to them directly from a local cache of values
+collected from the game server every 10 seconds.
+
+This issue has been incorrectly identified at times as being caused by provider
+DDoS protection, most notibly OVH.
+
+# Risks
+
+There is a chance that the server information could be up to 10 seconds old
+which might be flagged by the steam master server as spoofed. Use this at your
+own risk.
+
+# Method of operation
+
+At startup and every 10 seconds there after the application attempts to connect
+to the game server directly and collect the `A2S_INFO`, `A2S_PLAYER`, and
+`A2S_RULES` details which it then caches into ram.
+
+Incoming packets from clients are collected by the application via the NFQUEUE C
+interface, which is then parsed for Steam Query protocol packets, anything that
+does not match is passed through to the backend application without modification.
+
+When a steam query message is identified to be handled the application manually
+crafts a valid reply packet with the appropriate information and sends this to
+the client. The intercepted packet is dropped preventing it from reaching the
+application.
+
+# Limitations
+
+At current this project does not work with split packet messages, as such
+datagrams that exceed 1400 bytes will likely cause issues on servers with long
+descriptions or large player counts until this is addressed. This is not a
+technical limitation, it just simply has not yet been implemented yet.
+
+# Building
+
+## Dependencies
+
+* cmake
+* gcc
+* libnetfilter-queue-dev
+* libmnl-dev
+
+## Compiling
+
+```
+mkdir build
+cd build
+cmake ../
+make
+```
+
+# Usage
+
+Add a iptables nfqueue target that processes every incoming packet for the query
+port (usually 27015), I recommend using the following rule which will fail safe
+even if this application is not running:
+
+    iptables -I INPUT 1 -p udp --dport 27015 -j NFQUEUE --queue-bypass --queue-num 0
+
+After this has been done simply start SteamQueryProxy for the correct queue on
+the game port, for example:
+
+    ./SteamQueryProxy 0 27105
+
+You should start to see messages such as the following...
+
+    Got S2C_CHALLENGE:: 0x1545279c
+    Got A2S_INFO_REPLY: 203 bytes
+    Got A2S_PLAYER_REPLY: 40 bytes
+    Got A2S_RULES_REPLY: 1258 bytes
+    A2S_INFO 0x4c1eeb6b
+    A2S_INFO 0x86be12d4
+    A2S_PLAYER 0x4c1eeb6b
+    A2S_INFO 0x1a6bb070
+    A2S_RULES 0x4c1eeb6b
+    A2S_INFO 0x80b77a95
+    A2S_INFO 0xb3a8b7ce
+    A2S_INFO 0x8f892200
+    A2S_INFO 0x064e69ae
+    A2S_INFO 0x9eec7936
+    A2S_INFO 0x5f934826
+    A2S_INFO 0x8888ab3a
+    A2S_PLAYER 0x5f934826
+    A2S_RULES 0x5f934826
+    A2S_INFO 0xc68183a1
+    A2S_INFO 0xd3e0952d
+    A2S_INFO 0xafcf3596
+    A2S_INFO 0xff49c049
+
+Messages starting with `Got` are sucessful requests for the actual server
+information from the game server which are cached in ram. All other messages are
+intercepted messages which have been handled by this application and the
+security challenge that has been used for the request.
+
+# Donations
+
+If you like this project and find it useful and would like to help out you can
+support this project directly by using the following platforms.
+
+* [GitHub](https://github.com/sponsors/gnif)
+* [Ko-Fi](https://ko-fi.com/lookingglass)
+* [Patreon](https://www.patreon.com/gnif)
+* [Paypal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ESQ72XUPGKXRY)
+* BTC - 14ZFcYjsKPiVreHqcaekvHGL846u3ZuT13
+* ETH - 0x6f8aEe454384122bF9ed28f025FBCe2Bce98db85
+
+# License
+
+MIT License
+
+Copyright (c) 2022 Geoffrey McRae
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/main.c
@@ -0,0 +1,632 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <time.h>
+#include <limits.h>
+#include <linux/types.h>
+
+#include <libmnl/libmnl.h>
+#include <linux/netfilter.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
+static int                 g_queryPort;
+static struct mnl_socket * nl;
+static int                 g_socket;
+
+// retain 60s worth of challenges
+static uint32_t g_challenges[6] = { 0 };
+static uint32_t g_challengeIndex = -1;
+
+atomic_flag dataLock;
+#define LOCK(x) \
+  while(atomic_flag_test_and_set_explicit(&(x), memory_order_acquire)) { ; }
+#define UNLOCK(x) \
+  atomic_flag_clear_explicit(&(x), memory_order_release);
+
+typedef struct
+{
+  void         * data;
+  unsigned int   len;
+}
+Cache;
+
+Cache a2sInfo   = { 0 };
+Cache a2sPlayer = { 0 };
+Cache a2sRules  = { 0 };
+
+#define S2C_CHALLENGE                0x41
+#define A2S_PLAYER_REPLY             0x44
+#define A2S_RULES_REPLY              0x45
+#define A2S_INFO_REPLY               0x49
+#define A2S_INFO                     0x54
+#define A2S_PLAYER                   0x55
+#define A2S_RULES                    0x56
+#define A2A_PING                     0x69
+#define A2S_SERVERQUERY_GETCHALLENGE 0x57
+
+struct pseudo_header
+{
+	u_int32_t source_address;
+	u_int32_t dest_address;
+	u_int8_t placeholder;
+	u_int8_t protocol;
+	u_int16_t udp_length;
+};
+
+uint16_t csum(void *buf, int cb)
+{
+  uint16_t *ptr = (uint16_t *)buf;
+  int32_t sum = (cb & 1) ? ((uint8_t *)buf)[cb - 1] : 0;
+  cb /= 2;
+  while(cb--)
+    sum += *ptr++;
+  return (uint16_t)~((sum >> 16) + (sum & 0xffff));
+}
+
+void sendPacket(
+  uint32_t daddr, uint32_t saddr,
+  uint16_t dport, uint16_t sport,
+  void *   data , uint16_t len)
+{
+  const int size = sizeof(struct iphdr) + sizeof(struct udphdr) + len;
+
+  char datagram[size];
+  memset(datagram, 0, size);
+
+  struct iphdr  * iph = (struct iphdr *)datagram;
+  struct udphdr * udp = (struct udphdr *)(iph + 1);
+  char * payload      = (char *)(udp + 1);
+
+  memcpy(payload, data, len);
+  iph->ihl      = 5;
+  iph->version  = 4;
+  iph->tos      = 0;
+  iph->tot_len  = size;
+  iph->id       = htonl(54321);
+  iph->frag_off = 0;
+  iph->ttl      = 255;
+  iph->protocol = IPPROTO_UDP;
+  iph->check    = 0;
+  iph->saddr    = saddr;
+  iph->daddr    = daddr;
+  iph->check    = csum((unsigned short *)datagram, sizeof(*iph));
+
+  udp->source = sport;
+  udp->dest   = dport;
+  udp->len    = htons(8 + len);
+  udp->check  = 0;
+
+  {
+    char psuedogram[sizeof(struct pseudo_header) + sizeof(*udp) + len];
+    struct pseudo_header * psh = (struct pseudo_header *)psuedogram;
+    psh->source_address = saddr;
+    psh->dest_address   = daddr;
+    psh->placeholder    = 0;
+    psh->protocol       = IPPROTO_UDP;
+    psh->udp_length     = htons(sizeof(struct udphdr) + len);
+    memcpy(psh + 1, udp, sizeof(*udp) + len);
+    udp->check = csum((unsigned short *)psuedogram, sizeof(psuedogram));
+  }
+
+  struct sockaddr_in sin =
+  {
+    .sin_family      = AF_INET,
+    .sin_port        = dport,
+    .sin_addr.s_addr = daddr
+  };
+
+  sendto(g_socket, datagram, sizeof(datagram), 0,
+      (struct sockaddr *)&sin, sizeof(sin));
+}
+
+static void newChallenge(void)
+{
+  int next = g_challengeIndex + 1;
+  if (next == sizeof(g_challenges) / sizeof(*g_challenges))
+    next = 0;
+
+  do
+  {
+    g_challenges[next] = rand() % UINT32_MAX;
+    if (g_challenges[next] == 0 || g_challenges[next] == 0xFFFFFFFF)
+      continue;
+  }
+  while(false);
+
+  g_challengeIndex = next;
+}
+
+static bool validateChallenge(uint32_t challenge, uint32_t mutate)
+{
+  if (challenge == 0xFFFFFFFF)
+    return false;
+
+  int index = g_challengeIndex;
+  challenge ^= mutate;
+  for(int i = 0; i < sizeof(g_challenges) / sizeof(*g_challenges); ++i)
+  {
+    if (g_challenges[index] == challenge)
+      return true;
+
+    if (index == 0)
+      index = (sizeof(g_challenges) / sizeof(*g_challenges)) - 1;
+    else
+      --index;
+
+    if (g_challenges[index] == 0)
+      return false;
+  }
+
+  return false;
+}
+
+static void sendChallenge(struct iphdr * iph, struct udphdr * udp)
+{
+  char buffer[9];
+  memcpy(buffer, "\xFF\xFF\xFF\xFF\x41", 5);
+  uint32_t * challenge = (uint32_t *)(buffer + 5);
+  *challenge = g_challenges[g_challengeIndex] ^ (iph->saddr + udp->source);
+
+  sendPacket(
+    iph->saddr,
+    iph->daddr,
+    udp->source,
+    udp->dest,
+    buffer,
+    sizeof(buffer));
+}
+
+static bool parse_payload(void * payload, uint16_t len)
+{
+  if (len < sizeof(struct iphdr) + sizeof(struct udphdr))
+    return true;
+
+  struct iphdr * iph = (struct iphdr *)payload;
+  if (iph->protocol != IPPROTO_UDP)
+    return true;
+
+  struct udphdr * udp = (struct udphdr *)(iph + 1);
+  payload = (void *)(udp + 1);
+
+#if 0
+  char saddr[INET_ADDRSTRLEN];
+  char daddr[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &iph->saddr, saddr, sizeof(saddr));
+  inet_ntop(AF_INET, &iph->daddr, daddr, sizeof(daddr));
+  printf("%s:%d -> %s:%d\n", saddr, ntohs(udp->source), daddr, ntohs(udp->dest));
+#endif
+
+  uint32_t * header = (uint32_t *)payload;
+  if (*header != 0xFFFFFFFF)
+    return true;
+
+  uint8_t * query = (uint8_t *)(header + 1);
+  switch(*query)
+  {
+    case A2S_INFO:
+    {
+      char * data = (char *)(query + 1);
+      if (memcmp(data, "Source Engine Query\0", 20) != 0)
+        break;
+
+      if (len == 53)
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      uint32_t * challenge = (uint32_t *)(data + 20);
+      if (!validateChallenge(*challenge, iph->saddr + udp->source))
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      printf("A2S_INFO 0x%08x\n", *challenge);
+      LOCK(dataLock);
+      sendPacket(
+        iph->saddr,
+        iph->daddr,
+        udp->source,
+        udp->dest,
+        a2sInfo.data,
+        a2sInfo.len);
+      UNLOCK(dataLock);
+      return false;
+    }
+
+    case A2S_PLAYER:
+    {
+      if (len == 33)
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      uint32_t * challenge = (uint32_t *)(query + 1);
+      if (!validateChallenge(*challenge, iph->saddr + udp->source))
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      printf("A2S_PLAYER 0x%08x\n", *challenge);
+      LOCK(dataLock);
+      sendPacket(
+        iph->saddr,
+        iph->daddr,
+        udp->source,
+        udp->dest,
+        a2sPlayer.data,
+        a2sPlayer.len);
+      UNLOCK(dataLock);
+      return false;
+    }
+
+    case A2S_RULES:
+    {
+      if (len == 33)
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      uint32_t * challenge = (uint32_t *)(query + 1);
+      if (!validateChallenge(*challenge, iph->saddr + udp->source))
+      {
+        sendChallenge(iph, udp);
+        return false;
+      }
+
+      printf("A2S_RULES 0x%08x\n", *challenge);
+      LOCK(dataLock);
+      sendPacket(
+        iph->saddr,
+        iph->daddr,
+        udp->source,
+        udp->dest,
+        a2sRules.data,
+        a2sRules.len);
+      UNLOCK(dataLock);
+      return false;
+    }
+
+    case A2A_PING:
+    {
+      printf("A2A_PING\n");
+      sendPacket(
+        iph->saddr,
+        iph->daddr,
+        udp->source,
+        udp->dest,
+        "\xFF\xFF\xFF\xFF\x6A" "00000000000000\0",
+        20);
+      return false;
+    }
+
+    case A2S_SERVERQUERY_GETCHALLENGE:
+    {
+      printf("A2S_SERVERQUERY_GETCHALLENGE\n");
+      sendChallenge(iph, udp);
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static void nfq_send_verdict(int queue_num, uint32_t id, int verdict)
+{
+	char buf[MNL_SOCKET_BUFFER_SIZE];
+	struct nlmsghdr *nlh;
+
+	nlh = nfq_nlmsg_put(buf, NFQNL_MSG_VERDICT, queue_num);
+	nfq_nlmsg_verdict_put(nlh, id, verdict);
+
+	if (mnl_socket_sendto(nl, nlh, nlh->nlmsg_len) < 0)
+  {
+		perror("mnl_socket_send");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static int queue_cb(const struct nlmsghdr *nlh, void *data)
+{
+	struct nlattr *attr[NFQA_MAX+1] = {};
+	if (nfq_nlmsg_parse(nlh, attr) < 0)
+  {
+		perror("problems parsing");
+		return MNL_CB_ERROR;
+	}
+
+	struct nfgenmsg *nfg = mnl_nlmsg_get_payload(nlh);
+	if (attr[NFQA_PACKET_HDR] == NULL)
+  {
+		fputs("metaheader not set\n", stderr);
+		return MNL_CB_ERROR;
+	}
+
+	struct nfqnl_msg_packet_hdr * ph = mnl_attr_get_payload(attr[NFQA_PACKET_HDR]);
+  uint32_t id = ntohl(ph->packet_id);
+
+	uint16_t plen    = mnl_attr_get_payload_len(attr[NFQA_PAYLOAD]);
+	void *   payload = mnl_attr_get_payload(attr[NFQA_PAYLOAD]);
+  int      verdict = parse_payload(payload, plen) ? NF_ACCEPT : NF_DROP;
+
+	nfq_send_verdict(ntohs(nfg->res_id), id, verdict);
+	return MNL_CB_OK;
+}
+
+int msleep(long msec)
+{
+  struct timespec ts;
+  int res;
+
+  if (msec < 0)
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  ts.tv_sec = msec / 1000;
+  ts.tv_nsec = (msec % 1000) * 1000000;
+
+  do {
+    res = nanosleep(&ts, &ts);
+  } while (res && errno == EINTR);
+
+  return res;
+}
+
+static void * queryThread(void * opaque)
+{
+  struct sockaddr_in sin =
+  {
+    .sin_family      = AF_INET,
+    .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+    .sin_port        = htons(g_queryPort)
+  };
+
+  socklen_t slen = sizeof(sin);
+  int       sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  bool     haveAnswer = false;
+  uint32_t answer     = 0;
+  for(;;)
+  {
+    newChallenge();
+
+    // initial query to get the challenge
+    {
+      uint8_t buffer[25 + (haveAnswer ? 4 : 0)];
+      uint32_t * header  = (uint32_t *)buffer;
+      uint8_t  * query   = (uint8_t  *)(header + 1);
+      char     * payload = (char     *)(query  + 1);
+
+      if (haveAnswer)
+      {
+        uint32_t * challenge = (uint32_t *)(payload + 20);
+        *challenge = answer;
+      }
+
+      *header = 0xFFFFFFFF;
+      *query  = A2S_INFO;
+      memcpy(payload, "Source Engine Query\0", 20);
+      sendto(sock, buffer, sizeof(buffer), 0,
+          (struct sockaddr *)&sin, sizeof(sin));
+    }
+
+read:
+    {
+      uint8_t buffer[1400];
+      int len;
+      if ((len = recvfrom(sock, buffer, sizeof(buffer), 0,
+          (struct sockaddr *)&sin, &slen)) == -1)
+      {
+        perror("recvfrom A2S_INFO");
+        goto loop;
+      }
+
+      uint32_t * header  = (uint32_t *)buffer;
+      uint8_t  * query   = (uint8_t  *)(header + 1);
+      switch(*query)
+      {
+        case S2C_CHALLENGE:
+        {
+          uint32_t * challenge = (uint32_t *)(query + 1);
+          answer     = *challenge;
+          haveAnswer = true;
+          printf("Got S2C_CHALLENGE:: 0x%08x\n", *challenge);
+          continue;
+        }
+
+        case A2S_INFO_REPLY:
+        {
+          LOCK(dataLock);
+          if (a2sInfo.data)
+            free(a2sInfo.data);
+
+          printf("Got A2S_INFO_REPLY: %d bytes\n", len);
+          a2sInfo.data = malloc(len);
+          a2sInfo.len  = len;
+          memcpy(a2sInfo.data, buffer, len);
+          UNLOCK(dataLock);
+
+          // request the player info
+          uint32_t * challenge = (uint32_t *)(query + 1);
+
+          *header    = 0xFFFFFFFF;
+          *query     = A2S_PLAYER;
+          *challenge = answer;
+
+          sendto(sock, buffer, 9, 0,
+              (struct sockaddr *)&sin, sizeof(sin));
+          goto read;
+        }
+
+        case A2S_PLAYER_REPLY:
+        {
+          LOCK(dataLock);
+          if (a2sPlayer.data)
+            free(a2sPlayer.data);
+
+          printf("Got A2S_PLAYER_REPLY: %d bytes\n", len);
+          a2sPlayer.data = malloc(len);
+          a2sPlayer.len  = len;
+          memcpy(a2sPlayer.data, buffer, len);
+          UNLOCK(dataLock);
+
+          // request the rules
+          uint32_t * challenge = (uint32_t *)(query + 1);
+
+          *header    = 0xFFFFFFFF;
+          *query     = A2S_RULES;
+          *challenge = answer;
+
+          sendto(sock, buffer, 9, 0,
+              (struct sockaddr *)&sin, sizeof(sin));
+          goto read;
+        }
+
+        case A2S_RULES_REPLY:
+        {
+          LOCK(dataLock);
+          if (a2sRules.data)
+            free(a2sRules.data);
+
+          printf("Got A2S_RULES_REPLY: %d bytes\n", len);
+          a2sRules.data = malloc(len);
+          a2sRules.len  = len;
+          memcpy(a2sRules.data, buffer, len);
+          UNLOCK(dataLock);
+          break;
+        }
+      }
+    }
+
+loop:
+    msleep(1000 * 10);
+  }
+
+  close(sock);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	char *buf;
+	/* largest possible packet payload, plus netlink data overhead: */
+	size_t sizeof_buf = 0xffff + (MNL_SOCKET_BUFFER_SIZE/2);
+	struct nlmsghdr *nlh;
+	int ret;
+	unsigned int portid, queue_num;
+
+	if (argc != 3)
+  {
+		printf("Usage: %s [queue_num] [query_port]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	queue_num   = atoi(argv[1]);
+  g_queryPort = atoi(argv[2]);
+
+  if (g_queryPort < 1 || g_queryPort > 65535)
+  {
+    printf("Invalid query port\n");
+    exit(EXIT_FAILURE);
+  }
+
+	nl = mnl_socket_open(NETLINK_NETFILTER);
+	if (nl == NULL)
+  {
+		perror("mnl_socket_open");
+		exit(EXIT_FAILURE);
+	}
+
+	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0)
+  {
+		perror("mnl_socket_bind");
+		exit(EXIT_FAILURE);
+	}
+	portid = mnl_socket_get_portid(nl);
+
+	buf = malloc(sizeof_buf);
+	if (!buf)
+  {
+		perror("allocate receive buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	nlh = nfq_nlmsg_put(buf, NFQNL_MSG_CONFIG, queue_num);
+	nfq_nlmsg_cfg_put_cmd(nlh, AF_INET, NFQNL_CFG_CMD_BIND);
+
+	if (mnl_socket_sendto(nl, nlh, nlh->nlmsg_len) < 0)
+  {
+		perror("mnl_socket_send");
+		exit(EXIT_FAILURE);
+	}
+
+	nlh = nfq_nlmsg_put(buf, NFQNL_MSG_CONFIG, queue_num);
+	nfq_nlmsg_cfg_put_params(nlh, NFQNL_COPY_PACKET, 0xffff);
+
+	mnl_attr_put_u32(nlh, NFQA_CFG_FLAGS, htonl(NFQA_CFG_F_GSO));
+	mnl_attr_put_u32(nlh, NFQA_CFG_MASK, htonl(NFQA_CFG_F_GSO));
+
+	if (mnl_socket_sendto(nl, nlh, nlh->nlmsg_len) < 0)
+  {
+		perror("mnl_socket_send");
+		exit(EXIT_FAILURE);
+	}
+
+	/* ENOBUFS is signalled to userspace when packets were lost
+	 * on kernel side.  In most cases, userspace isn't interested
+	 * in this information, so turn it off.
+	 */
+	ret = 1;
+	mnl_socket_setsockopt(nl, NETLINK_NO_ENOBUFS, &ret, sizeof(int));
+
+  g_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+
+  srand(time(NULL));
+  newChallenge();
+
+  atomic_flag_clear(&dataLock);
+  pthread_t qt;
+  pthread_create(&qt, NULL, queryThread, NULL);
+
+  /* wait until we have all the information needed to operate */
+  while(!a2sInfo.len || !a2sPlayer.len || !a2sRules.len)
+    msleep(200);
+
+	for (;;)
+  {
+		ret = mnl_socket_recvfrom(nl, buf, sizeof_buf);
+		if (ret == -1)
+    {
+			perror("mnl_socket_recvfrom");
+			exit(EXIT_FAILURE);
+		}
+
+		ret = mnl_cb_run(buf, ret, 0, portid, queue_cb, NULL);
+		if (ret < 0)
+    {
+			perror("mnl_cb_run");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+  pthread_join(qt, NULL);
+  close(g_socket);
+	mnl_socket_close(nl);
+
+	return 0;
+}
